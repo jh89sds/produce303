9c2f3668572d00de48b2a593fe570572959d7c9d
jh89sds <ljh89st@naver.com>
2016-04-17 00:40:28 +0900

    Index jade file change to jsx file. exclude idea(intellij config file) file and npm-debug file in git. add devDependencies such as babel. add dependencies such as express-react-views.

--- produce303/app.js
+++ produce303/app.js
@@ -14,7 +14,8 @@ var app = express();
 // all environments
 app.set('port', process.env.PORT || 3000);
 app.set('views', __dirname + '/views');
-app.set('view engine', 'jade');
+app.set('view engine', 'jsx');
+app.engine('jsx', require('express-react-views').createEngine());
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
